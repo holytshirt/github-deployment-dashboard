6fdc6afcd9ac0935074f072149b6cc9bf8738176
Toby Henderson <hendersont@gmail.com>
2024-09-06 16:48:03 +0100

    fix: disable image optimization for static export
    - Added `unoptimized: true` to images config in next.config.js
    - Resolves build error related to Image Optimization API incompatibility with static exports

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  env: {
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME,
-  },
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
+    output: 'export',
+    env: {
+        GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+        GITHUB_USERNAME: process.env.GITHUB_USERNAME,
+    },
+    images: {
+        domains: ['avatars.githubusercontent.com'],
+        unoptimized: true,
+    },
 }
